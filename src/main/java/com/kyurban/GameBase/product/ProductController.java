434fb67640471751a65172a7372f8a77a333e20f
kurban15 <kyleurban99@gmail.com>
2025-02-16 22:35:18 -0800

    Completed the Product class

--- src/main/java/com/kyurban/GameBase/product/ProductController.java
+++ src/main/java/com/kyurban/GameBase/product/ProductController.java
@@ -1,4 +1,23 @@
 package com.kyurban.GameBase.product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
+@RestController
+@RequestMapping(path = "api/product")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return productService.getProducts();
+    }
 }
--- src/main/java/com/kyurban/GameBase/product/ProductService.java
+++ src/main/java/com/kyurban/GameBase/product/ProductService.java
@@ -1,4 +1,13 @@
 package com.kyurban.GameBase.product;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
+@Service
 public class ProductService {
+
+    public List<Product> getProducts() {
+        return List.of(new Product(1, "headphones", 30.00, 10));
+    }
+
 }
